028c28893ab0075abb91a3a389c8289c7501b9b0
Vinh Nguyen <vinhnguyen2308@gmail.com>
2012-09-03 16:03:34 +0100

    remove unused

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-#init
 require 'rubygems'
 require 'twitter'
 require 'sinatra'
 require 'haml'
 
-# Twitter auth
 Twitter.configure do |config|
   config.consumer_key = 'DZDpYsau2bElVwwsLicAkA'
   config.consumer_secret = '7hsKYBavZDXPnNMlFYY20bxG3FQBfDByBoat4k74'
@@ -12,31 +10,24 @@ Twitter.configure do |config|
   config.oauth_token_secret = 'ab7gl6SWsbhvXIkdgRtJzlnScpx45u81nQMR3C9fbY'
 end
 
-# define stuffs
 def twitter_id(screen_name)
   Twitter.user(screen_name).id
 end
 
-  # like it sound
 def is_following?(a,b)
   followers = Twitter.follower_ids(twitter_id(b)).ids
   followers.include?(twitter_id(a))
 end
   
-  # user's location
 def location_u(screen_name)
   location = Twitter.user(screen_name).location
 end
 
-  # most recent tweet
 def recent_tweet(screen_name)
   recent_tweet = Twitter.user_timeline(screen_name).first.text
 end
 
-#page binding
 get '/' do
-  # haml
-  # default index.haml
   haml :index
 end
 
@@ -44,15 +35,12 @@ get '/check' do
   @user1 = params[:user1]
   @user2 = params[:user2]
 
-  # following check
   @following1 = is_following?(@user1, @user2)
   @following2 = is_following?(@user2, @user1)
 
-  # locations detect
   @location1 = location_u(@user1)
   @location2 = location_u(@user2)
 
-  # latest tweet
   @recent_tweet1 = recent_tweet(@user1)
   @recent_tweet2 = recent_tweet(@user2)
 
